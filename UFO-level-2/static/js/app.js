990a43a0c8a4d6c4314f509a308ab53538bf0192
sagastya <sagastya@yahoo.com>
2020-02-29 20:52:49 -0500

    making it better

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -9,47 +9,89 @@ createTable(tableData);
 // Handle 'Filter Table' button click event
 var button = d3.select("#filter-btn");
 button.on("click", buttonClick);
+button.on("change", inputChange);
 
 // This function creates UFO sightings table
-function createTable(table){
-    // Get a reference to the table body
-    var tbody = d3.select("tbody");
-  
-    // Clear the table data
-    tbody.html("");
-  
-    // Create a table and add the ufo sightings
-    table.forEach((ufoReport) => {
-      var row = tbody.append("tr");
-      Object.entries(ufoReport).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value); });
+function createTable(table) {
+  // Get a reference to the table body
+  var tbody = d3.select("tbody");
+
+  // Clear the table data
+  tbody.html("");
+
+  // Create a table and add the ufo sightings
+  table.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
     });
+  });
 }
 
 // This function is triggered when the 'Filter Table' button is clicked
 function buttonClick() {
-//   console.log("The Date button was clicked!");
+  //   console.log("The Date button was clicked!");
 
   // We can use d3 to see the object that dispatched the event
-//   console.log(d3.event.target);
+  //   console.log(d3.event.target);
 
-  // Select the input date element
-  var inputElement = d3.select("#datetime");
-
-  // Get the value property of the input date
-  var inputDate = inputElement.property("value");
-//   console.log(inputDate);
+  // Get the value property of the input elements
+  var inputDate = d3.select("#datetime").property("value");
+  var inputCity = d3.select("#city").property("value");
+  var inputState = d3.select("#state").property("value");
+  var inputCountry = d3.select("#country").property("value");
+  var inputShape = d3.select("#shape").property("value");
+  // console.log(inputDate);
+  // console.log(inputCity);
+  // console.log(inputState);
+  // console.log(inputCountry);
+  // console.log(inputShape);
 
   // Filter sightings for input date 
   if (inputDate) {
     // filter table data by input date
-    result = tableData.filter(sighting => sighting.datetime === inputDate);
-    // console.log(result);  
-    createTable(result);
+    filteredData = tableData.filter(sighting => sighting.datetime === inputDate);
+    console.log(filteredData);
+  };
+  if (inputCity && filteredData) {
+    filteredData = filteredData.filter(sighting => sighting.city === inputCity);
+    console.log(filteredData);
+  } else if (inputCity) {
+    filteredData = tableData.filter(sighting => sighting.city === inputCity);
+    console.log(filteredData);
+  };
+  if (inputState && filteredData) {
+    filteredData = filteredData.filter(sighting => sighting.state === inputState);
+    console.log(filteredData);
+  } else if (inputState) {
+    filteredData = tableData.filter(sighting => sighting.state === inputState);
+    congsole.log(filteredData);
+  };
+  if (inputCountry && filteredData) {
+    filteredData = filteredData.filter(sighting => sighting.country === inputCountry);
+    console.log(filteredData);
+  } else if (inputCountry) {
+    filteredData = tableData.filter(sighting => sighting.country === inputCountry);
+    console.log(filteredData);
+  };
+  if (inputShape && filteredData) {
+    filteredData = filteredData.filter(sighting => sighting.shape === inputShape);
+    console.log(filteredData);
+  } else if (inputShape) {
+    filteredData = tableData.filter(sighting => sighting.shape === inputShape);
+    console.log(filteredData);
+  };
+  if (filteredData) {
+    createTable(filteredData);
   } else {
-      d3.select("tbody").html("");
-  }
+    d3.select("tbody").html("");
+  };
+}
+
+function inputChange() {
+  d3.select("tbody").html("");
+  button.on("click", buttonClick);
 }
 
 
